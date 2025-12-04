664441c7a9e989bbbd802179829b850d94f57432
Salman Baihaqi <134832455+wrldwideee@users.noreply.github.com>
2025-12-04 23:04:09 +0700

    v1

---
+++ resto.h
@@ -0,0 +1,38 @@
+#ifndef RESTO_H_INCLUDED
+#define RESTO_H_INCLUDED
+#include <iostream>
+using namespace std;
+
+typedef struct Menu* adrMenu;
+typedef struct Resto* adrResto;
+
+struct Menu{
+    string namaMenu;
+    int harga;
+    string kategoriMenu;
+    adrMenu nextChild;
+};
+
+struct Resto{
+    int idResto;
+    string namaResto;
+    string alamat;
+    string kategoriResto;
+    adrMenu firstChild;
+    adrResto nextParent;
+};
+
+struct List{
+    adrResto first;
+};
+
+void createList(List &L);
+adrResto createResto(int id, string name, string alamat, string kategoriResto);
+adrMenu createMenu(string nama, int harga, string kategoriMenu);
+void insertResto(List &L, adrResto p);
+void deleteResto(List &L, int id);
+adrResto searchResto(List L, int id);
+
+
+
+#endif // RESTO_H_INCLUDED
